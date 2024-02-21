ad566b8d0d691306bd6fbd10321e43354aee9784
BPsaren <bishnupadasaren78@gmail.com>
2024-02-21 07:28:06 +0530

    16. Problem: MongoDB Connection Setup

--- Day-16/sixtheen.js
+++ Day-16/sixtheen.js
@@ -1,37 +1,23 @@
+require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
 
-// MongoDB connection string
-const mongoURI = 'mongodb://127.0.0.1/mydatabase';
+const mongoURI = process.env.mongoDB_URI;
 
-/**
- * Establishes a connection to MongoDB using Mongoose
- */
 function connectToMongoDB() {
-  mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-  const db = mongoose.connection;
-
-  db.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
-  });
-
-  db.once('open', () => {
-    console.log('Successfully connected to MongoDB');
-  });
+  mongoose.connect(mongoURI)
+    .then(() => console.log('Successfully connected to MongoDB'))
+    .catch(error => console.error('MongoDB connection error:', error));
 }
 
-// Call the function to connect to MongoDB
 connectToMongoDB();
 
-// Example route
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('30 days nodejs');
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
